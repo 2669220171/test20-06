7064daec9cafa5ac29fa37074c72d7ef753f7ee3
lyw <18220539776@163.com>
2021-06-03 18:05:17 +0800

    二次上传

--- src/com/bjpoweernode/domain/Student.java
+++ src/com/bjpoweernode/domain/Student.java
@@ -3,7 +3,9 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Student {
-
+    public static void main(String[] args) {
+        System.out.println("Hello Word");
+    }
 }
 
  class Main {
